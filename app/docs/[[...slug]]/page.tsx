d7d8c8959b6f43494663b9fa556c68c6fbd5109a
yibeltal <yibeltal2514@gmail.com>
2025-12-03 14:38:43 +0300

    page not found

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -1,4 +1,5 @@
 import { features } from 'process'
+//catch all segment route
 import React from 'react'
 
 export default async function Docs({params,}
@@ -7,14 +8,14 @@ export default async function Docs({params,}
     
 }) {
    
-    if((await params).slug.length===2){
+    if((await params).slug?.length===2){
         return(
             <h1>
                 Views docs for feature {(await params).slug[0]} and concept {(await params).slug[1]}
             </h1>
         )
     }
-    else if((await params).slug.length===1){
+    else if((await params).slug?.length===1){
         return (
             <h1>
                 Viewing docs for  {(await params).slug[0]}
---
+++ app/not-found.tsx
@@ -0,0 +1,13 @@
+ import React from 'react'
+ 
+ export default function NotFound() {
+   return (
+   <div>
+        <h1>Page not Not found</h1>
+        <p>Cloud not find requested resource</p>
+      </div>
+   )
+ }
+ 
+ 
+  
