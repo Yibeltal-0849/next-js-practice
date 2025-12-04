370d6922d5c63eace93dc476bcbe8719441b8f54
yibeltal <yibeltal2514@gmail.com>
2025-12-04 10:17:21 +0300

    route metadata , static and dynamic metadata

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,9 @@
+import { title } from 'process'
 import React from 'react'
-
+export const metadata={
+  title:"Next.js",
+  description:"Coed Evalution"
+}
 export default function About() {
   return (
     <div>
--- app/products/[productId]/page.tsx
+++ app/products/[productId]/page.tsx
@@ -1,15 +1,51 @@
 
-//dynamic route
+// //dynamic route
 
-export default async function ProductDetail({ params }: {
-  params: Promise<{ productId: string }>
-}) {
+// export default async function ProductDetail({ params }: {
+//   params: Promise<{ productId: string }>
+// }) {
 
+//   const { productId } = await params;
+
+//   return (
+//     <div>
+//       <h1>product detail {productId}</h1>
+//     </div>
+//   );
+// }
+
+// dynamic route
+// app/products/[productId]/page.tsx
+import { Metadata } from "next";
+
+type Props = {
+  params: Promise<{ productId: string }>;
+};
+
+// Dynamic Metadata
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { productId } = await params;
+
+  const title = await new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${productId}`);
+    }, 100);
+  });
+
+  return {
+    title: `Product ${title}`,
+  };
+}
+
+// Page Component
+export default async function ProductDetail({ params }: Props) {
   const { productId } = await params;
 
   return (
     <div>
-      <h1>product detail {productId}</h1>
+      <h1>Product detail {productId}</h1>
     </div>
   );
 }
+
+
